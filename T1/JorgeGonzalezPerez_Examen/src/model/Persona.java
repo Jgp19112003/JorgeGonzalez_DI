e673e01f7c86fe49d45f8c4760995b5ac63cc32e
Jorge_González-dev <jgp19112003@gmail.com>
2022-09-26 11:13:54 +0200

    .

---
+++ T1/JorgeGonzalezPerez_Examen/src/model/Persona.java
@@ -0,0 +1,40 @@
+package model;
+
+public class Persona {
+
+   private String nombre, apellido, DNI;
+
+    public Persona() {
+    }
+
+    public Persona(String nombre, String apellido, String DNI) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.DNI = DNI;
+    }
+
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getDNI() {
+        return DNI;
+    }
+
+    public void setDNI(String DNI) {
+        this.DNI = DNI;
+    }
+}
