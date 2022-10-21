fe85d89b5a3fba44de039254471c1c29a8bd28ad
Jgp19112003 <92918174+Jgp19112003@users.noreply.github.com>
2022-10-21 17:54:04 +0200

    .

--- T2/T4/Calculadora/src/main/java/com/example/calculadora/MainController.java
+++ T2/T4/Calculadora/src/main/java/com/example/calculadora/MainController.java
@@ -1,4 +1,60 @@
 package com.example.calculadora;
 
-public class MainController {
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainController implements Initializable {
+
+    @FXML
+    GridPane gridPrincipal;
+
+    @FXML
+    TextArea textoOperacion;
+
+    @FXML
+    Button boton7;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        boton7.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                textoOperacion.setText(textoOperacion.getText()+boton7.getText());
+            }
+        });
+        acciones();
+    }
+
+    private void acciones() {
+        boton7.setOnAction(new ManejoPulsaciones());
+        for (Node child : gridPrincipal.getChildren()) {
+            if (child instanceof Button) {
+                ((Button) child).setOnAction(new ManejoPulsaciones());
+                textoOperacion.setText(textoOperacion.getText() + ((Button) child).getText());
+            }
+        }
+    }
+
+    class ManejoPulsaciones implements EventHandler<ActionEvent> {
+
+        @Override
+        public void handle(ActionEvent actionEvent) {
+            if (actionEvent.getSource() == boton7) {
+                System.out.println("Recibido");
+                textoOperacion.setText("Funciona");
+                System.out.println(textoOperacion.getText());
+            }
+        }
+
+
+    }
 }
--- T2/T4/Calculadora/src/main/java/module-info.java
+++ T2/T4/Calculadora/src/main/java/module-info.java
@@ -5,11 +5,9 @@ module com.example.calculadora {
 
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
-    requires validatorfx;
+
     requires org.kordamp.ikonli.javafx;
     requires org.kordamp.bootstrapfx.core;
-    requires eu.hansolo.tilesfx;
-    requires com.almasb.fxgl.all;
 
     opens com.example.calculadora to javafx.fxml;
     exports com.example.calculadora;
