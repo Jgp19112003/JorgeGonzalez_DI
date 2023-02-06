9d7418c998582c44fae6323c5d2fb370c85824d3
Jorge_González-dev <jgp19112003@gmail.com>
2023-02-06 14:29:15 +0100

    .

--- T3/T6/Proyectos_Angular/inicio/src/app/app.module.ts
+++ T3/T6/Proyectos_Angular/inicio/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicialComponent } from './components/inicial/inicial.component';
+import { SecundarioComponent } from './components/secundario/secundario.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicialComponent,
+    SecundarioComponent
   ],
   imports: [
     BrowserModule,
