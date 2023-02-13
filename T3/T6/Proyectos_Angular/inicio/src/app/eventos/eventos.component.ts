5282a8d4cc7a30165bdc5d3fcd69275ccb2cde81
Jorge_González-dev <jgp19112003@gmail.com>
2023-02-13 14:26:09 +0100

    .

--- T3/T6/Proyectos_Angular/inicio/src/app/eventos/eventos.component.ts
+++ T3/T6/Proyectos_Angular/inicio/src/app/eventos/eventos.component.ts
@@ -7,10 +7,14 @@ import { Component } from '@angular/core';
 })
 export class EventosComponent {
   alumno = {
-    nombre: 'Borja',
-    apellido: 'Martin',
-    edad: 38,
+    nombre: '',
+    apellido: '',
+    ciclo: "",
+    experiencia: "",
+    estudios:""
   };
+
+  estudios = ""
   cicloSeleccionado: string = "";
   ciclos = [
     "Bachillerato",
@@ -21,14 +25,26 @@ export class EventosComponent {
   urlImagen =
     'https://digital55.com/wp-content/uploads/2022/01/Renderizado_en_servidor_con_angular_universal.png';
 
-  pulsacionBotonIncial(nombre: string) {
-    //console.log(`Nombre pasado: ${nombre}, edad pasada ${edad}`);
+  pulsacionBotonIncial(nombre: string, apellido: string, ciclo: string, experiencia: boolean) {
+
     this.alumno.nombre = nombre;
-    console.log(nombre);
+    this.alumno.apellido = apellido;
+    this.alumno.ciclo = ciclo;
+    this.alumno.estudios = this.estudios
+    if(experiencia){
+    this.alumno.experiencia = "Tiene experiencia"
+    } else {
+      this.alumno.experiencia = "No tiene experiencia"
+    }
   }
 
-  radioListener(event: any){
-    this.cicloSeleccionado = event.value
+  capturaCambioRadio(estudios: string) {
+    this.estudios = estudios
+  }
+
+  capturaEvento(evento: any){
+    console.log(evento.target.value);
+
   }
 
 }
